5a981cf80ee9d16633609c4197d8ff598a99f8a9
Farawaystar Muhammad <77168999+farawaystar@users.noreply.github.com>
2024-11-15 16:30:23 +0100

    Update compare_2_csv_s.py

--- compare_2_csv_s.py
+++ compare_2_csv_s.py
@@ -5,8 +5,8 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', -1)
 
-f1 = pd.read_csv('members_IGMC2.csv')
-f2 = pd.read_csv('members_IMG.csv')
+f1 = pd.read_csv('members_channel1.csv')
+f2 = pd.read_csv('members_channel2.csv')
 
 # rows present in 2nd file missing in 1st
 print(f2[~f2.userid.isin(f1.userid)])
