1bf9cf20c9c20f1d1365141595f84d278df5c619
Farawaystar Muhammad <77168999+farawaystar@users.noreply.github.com>
2024-11-15 16:27:37 +0100

    Update concatenate_csv_cols.py

--- concatenate_csv_cols.py
+++ concatenate_csv_cols.py
@@ -6,6 +6,6 @@ pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', -1)
 
 df = pd.read_csv('file.csv', index_col='userid')
-res = df[list('ZabeerZakariyaGermany')].astype(str).sum(1)
+res = df[list('user_name')].astype(str).sum(1)
 
 print(res)
